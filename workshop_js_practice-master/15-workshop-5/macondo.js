cb9c911b2d2e557b915c7ec4d304aa1bd5449366
samuelacevedo9307 <110944010+samuelacevedo9307@users.noreply.github.com>
2024-04-25 23:53:40 -0500

    change 25  abril workshop 5

---
+++ workshop_js_practice-master/15-workshop-5/macondo.js
@@ -0,0 +1,93 @@
+//------------------------------- CASO1
+// const cost_hindebrando = 2500000;
+
+// var products_medellin = {
+//     almojábana_con_gaseosa: 15000,
+//     subway_con_gaseosa: 23000,
+// }
+
+// const compra_medellin = prompt("que desea solicitar hindebrando? \n1-almojábana con gaseosa \n2-subway con gaseosa \n3-No compra nada");
+
+// switch (compra_medellin) {
+//     case "1" : {
+        
+//         total_cost =cost_hindebrando - products_medellin.almojábana_con_gaseosa;
+//         console.log("su compra fue una almojábana con gaseosa");
+//         console.log(`su total actual es de ${total_gasto}`);
+//         break;
+//     }
+//     case "2" : {
+        
+//         total_cost =cost_hindebrando - products_medellin.subway_con_gaseosa;
+//         console.log("su compra fue una almojábana con gaseosa");
+//         console.log(`su total actual es de ${total_gasto}`);
+//         break;
+//     }
+//     case "3" : {
+//         console.log("Gracias vuelva pronto ");
+//         break;
+//     }
+
+// }
+//---------------------------------------------
+//------------------------------- CASO2
+//tamaño maleta aeropuesto
+// const  height_bag_airpot = 55 ;
+// const  large_bag_airpot = 40 ;
+// const width_bag_airpot = 20 ;
+// //tamaño maleta cliente
+// const height_bag = 60;
+// const large_bag = 40;
+// const width_bag = 20;
+// // factor de reduccion entre maletas
+// const reduction_height = height_bag_airpot/height_bag;
+// console.log(reduction_height);
+// const reduction_large = large_bag_airpot/large_bag;
+// console.log(reduction_large);
+// const reduction_width =width_bag_airpot/width_bag;
+// console.log(reduction_width);
+// //math min 
+// const math_reduction = Math.min (reduction_height,reduction_large,reduction_width);
+// console.log(math_reduction);
+// // nuevas dimesiones
+
+// const new_height = height_bag*math_reduction ;
+// console.log("nuevas dimensiones bag height "+new_height);
+// const new_large = large_bag*math_reduction ;
+// console.log("nuevas dimensiones bag large "+new_large);
+// const new_width = width_bag*math_reduction ;
+// console.log("nuevas dimensiones bag width "+new_width);
+//-------------------------------------------------------
+ //------------------------------- CASO3
+// const wifi_red_binary = "01110010_01101001_01110111_01101001";
+
+// const array_binary = [128 , 64 , 32 , 8 , 4 , 2 ,1];
+
+
+// function binary_to_decimal (array_binary.length) {
+// let string_pass = "";
+// let passcode = "";
+
+// const password = "01110010_01101001_01110111_01101001";
+// const split_password = password.split("_");
+
+// split_password.forEach( i => {
+//     passcode=parseInt(i,2);
+//     string_pass = string_pass + String.fromCharCode(passcode);
+//     console.log(string_pass);
+// });
+ //------------------------------- CASO4
+
+const text_taxi = "Taxi me puede llevar al hotel mariposas amarillas";
+const word_phrase_taxi = text_taxi.split("");
+console.log(word_phrase_taxi);
+
+word_phrase_taxi.forEach( i => {
+    
+});
+ //------------------------------- CASO5
+
+
+
+
+ //------------------------------- CASO6
