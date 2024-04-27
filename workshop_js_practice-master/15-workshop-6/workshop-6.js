b453a573bcbdbe6c60e2148a110839e67f1c46e7
samuelacevedo9307 <samuelacevedo9307@gmail.com>
2024-04-26 20:28:49 -0500

    new_workshop_6

---
+++ workshop_js_practice-master/15-workshop-6/workshop-6.js
@@ -0,0 +1,101 @@
+
+    // // Global Scope
+    // var globalVariable = "Soy una variable global.";
+
+    // function testScope() {
+    //   // Function Scope
+    //   var functionVariable = "Soy una variable local.";
+
+    //   if (true) {
+    //     // Block Scope
+    //     let blockVariable = "Soy una variable de bloque.";
+    //     console.log("Dentro del bloque:", blockVariable);
+    //   }
+
+    //   console.log("Dentro de la función:", functionVariable);
+    // }
+
+    // console.log("Fuera de la función:", globalVariable);
+    // testScope();
+//-------------------------EJERCICIO 1---------------------------------
+
+
+// var globalVariable = "Soy una variable global.";
+// //La variable llamada global tiene un ámbito global y por eso puede ser utilizada e incluso modificada dentro de la función
+//     function testScope() {
+//       // Function Scope
+//       var functionVariable = "Soy una variable local.";
+// //Solo se puede acceder a ellos desde dentro de la función.
+//       if (true) {
+//         // Block Scope
+//         let blockVariable = "Soy una variable de bloque.";
+//         console.log("Dentro del bloque:", blockVariable);
+//       }
+// //Una sentencia block se utiliza para agrupar cero o más sentencias. Este grupo block se delimita por un par de llaves.
+//       console.log("Dentro de la función:", functionVariable);
+//     }
+//     console.log("Fuera de la función:", globalVariable);
+//     testScope();
+
+// var selection_var = prompt ("las variables globales son:  \n-1 tiene un ámbito global y por eso puede ser utilizada e incluso modificada dentro de la función\n-2 se utiliza para agrupar cero o más sentencias. Este grupo block se delimita por un par de llaves.");
+
+// switch (selection_var) {
+//     case "1":
+//         alert("es correcto")
+//         break;
+//     case "2":
+//         alert("falso , intentalo nuevamente")
+//         break;
+// }
+// var selection_block =  prompt("las variables de bloque son  \n-1 se utiliza para agrupar cero o más sentencias. Este grupo block se delimita por un par de llaves. \n-2 Solo se puede acceder a ellos desde dentro de la función. ");
+// switch (selection_block) {
+//     case "1":
+//         alert("es correcto")
+//         break;
+//     case "2":
+//         alert("falso , intentalo nuevamente")
+//         break;
+// }
+
+// var selection_local =  prompt("las variables de bloque son  \n-1 Solo se puede acceder a ellos desde dentro de la función. \n-2 tiene un ámbito global y por eso puede ser utilizada e incluso modificada dentro de la función");
+// switch (selection_local) {
+//     case "1":
+//         alert("es correcto")
+//         break;
+//     case "2":
+//         alert("falso , intentalo nuevamente")
+//         break;
+// }
+//------------------------------EJERCICIO 2----------------------------
+    // // vars declaration
+    // var a = 1;
+    // let b = 2;
+    // const c = 3;
+
+    // // functions declarations
+    // function funcionDeclarada() {
+    //   return "Función declarada ha sido llamada.";
+    // }
+
+    // const funcionExpresada = function () {
+    //   return "Función expresada ha sido llamada.";
+    // };
+
+    // // vars call
+    // console.log("Valor de a:", a);
+    // console.log("Valor de b:", b);
+    // console.log("Valor de c:", c);
+
+    // // functions call
+    // console.log("Resultado de funcionDeclarada:", funcionDeclarada());
+    // console.log("Resultado de funcionExpresada:", funcionExpresada());
+
+
+
+    // variable en bloque 
+    // comportamiento hosting
+    // funciones declaradas y expresadas 
+    // que son clousures
+    //
+
+    //----------------------------------------------------------
