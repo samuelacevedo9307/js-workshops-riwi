99e87cfc5a4bd574154cc34b0e6ee6eea41d9683
samuelacevedo9307 <samuelacevedo9307@gmail.com>
2024-04-30 21:06:24 -0500

    change_30_abril

--- workshop_js_practice-master/15-workshop-6/workshop-6.js
+++ workshop_js_practice-master/15-workshop-6/workshop-6.js
@@ -53,7 +53,7 @@
 //         alert("es correcto")
 //         break;
 //     case "2":
-//         alert("falso , intentalo nuevamente")
+//         alert("falso ,abrace")
 //         break;
 // }
 
@@ -98,4 +98,53 @@
     // que son clousures
     //
 
-    //----------------------------------------------------------
+const selection_local = prompt(`el codigo mostrado en la pantalla es : \n1-Es correcto \n2-es incorrecto `);
+        
+switch (selection_local) {
+    case "1":
+        alert("no hay ningun tipo de error y toda la funcion se muestra resultado")
+        break;
+    case "2":
+        alert("hey errores evidentes en el codigo ")
+        const selection_second_local = prompt(`cuales errores notaste \n1-las variables declaradas con var son inicializadas con undefined, mientras que las variables con let y const no pueden ser accedidas antes de su declaración. \n2- las variables declaradas con var NO son inicializadas con undefined, mientras que las variables con let y const pueden ser accedidas antes de su declaración.`);
+        switch (selection_local) {
+            case "1":
+                alert (`es correcto , felicidades.`)
+                
+                break;
+            case "2":
+                alert (`es incorrecto Bruto de mierda , lea un libro , que pesar de su mama .`)
+                
+                break;
+        
+            default:
+                break;
+        }
+        break;
+}
+
+    // // vars call
+    // console.log("Valor de a:", a);
+    // console.log("Valor de b:", b);
+    // console.log("Valor de c:", c);
+
+    // // functions call
+    // console.log("Resultado de funcionDeclarada:", funcionDeclarada());
+    // console.log("Resultado de funcionExpresada:", funcionExpresada());
+
+    // // vars declaration
+    // var a = 1;
+    // let b = 2;
+    // const c = 3;
+
+    // // functions declarations
+    // function funcionDeclarada() {
+    //   return "Función declarada ha sido llamada.";
+    // }
+
+    // const funcionExpresada = function () {
+    //   return "Función expresada ha sido llamada.";
+    // };
+    
+
+    //-------------------------------Ejercicio 3: Closures en Acción---------------------------
